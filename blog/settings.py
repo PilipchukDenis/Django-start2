2822c361083f8a9270622bcf6ba0be0f1d93953f
PilipchukDenis <grutden@mail.ru>
2025-01-16 15:09:50 +0300

    class work

--- blog/settings.py
+++ blog/settings.py
@@ -51,7 +51,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
-
 ]
 
 ROOT_URLCONF = 'blog.urls'
@@ -133,6 +132,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
--- python_blog/models.py
+++ python_blog/models.py
@@ -84,4 +84,3 @@ class Category(models.Model):
         verbose_name = "Категория"
         verbose_name_plural = "Категории"
         ordering = ["name"]
-
